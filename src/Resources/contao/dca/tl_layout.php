65beb1f401bc6d7405016856f49324e82806d9ad
Daniel Bitsch <bitsch@leadingsystems.de>
2023-09-18 17:07:55 +0200

    optimize codestyling

--- src/Resources/contao/dca/tl_layout.php
+++ src/Resources/contao/dca/tl_layout.php
@@ -6,134 +6,113 @@ $GLOBALS['TL_DCA']['tl_layout']['config']['onsubmit_callback'][] = ['LeadingSyst
 
 $GLOBALS['TL_DCA']['tl_layout']['palettes']['default'] .= ';{lsjs4c_legend},lsjs4c_loadLsjs,lsjs4c_appToLoad,lsjs4c_appToLoadTextPath,lsjs4c_appCustomizationToLoad,lsjs4c_appCustomizationToLoadTextPath,lsjs4c_coreCustomizationToLoad,lsjs4c_coreCustomizationToLoadTextPath,lsjs4c_modulesToExclude,lsjs4c_modulesToExcludeTextPath,lsjs4c_debugMode,lsjs4c_noCache,lsjs4c_noMinifier';
 
-$GLOBALS['TL_DCA']['tl_layout']['fields']['lsjs4c_loadLsjs'] = array(
-    'sql'                     => "char(1) NOT NULL default ''",
-	'label'                   => &$GLOBALS['TL_LANG']['tl_layout']['lsjs4c_loadLsjs'],
-	'exclude'                 => true,
-	'inputType'               => 'checkbox',
-	'eval'                    => array('tl_class'=>'m12')
+$GLOBALS['TL_DCA']['tl_layout']['fields']['lsjs4c_loadLsjs'] = array
+(
+    'label'                   => &$GLOBALS['TL_LANG']['tl_layout']['lsjs4c_loadLsjs'],
+    'exclude'                 => true,
+    'inputType'               => 'checkbox',
+    'eval'                    => array('tl_class' => 'm12'),
+    'sql'                     => "char(1) NOT NULL default ''"
 );
 
-$GLOBALS['TL_DCA']['tl_layout']['fields']['lsjs4c_appToLoad'] = array(
-    'sql'                   => "blob NULL",
-	'label'                 => &$GLOBALS['TL_LANG']['tl_layout']['lsjs4c_appToLoad'],
-	'exclude'               => true,
-	'inputType'             => 'fileTree',
-	'eval'                  => array(
-		'multiple'              => false,
-		'tl_class'              =>'clr',
-		'files'                 => false,
-		'filesOnly'             => false,
-		'fieldType'             => 'radio'
-	)
+$GLOBALS['TL_DCA']['tl_layout']['fields']['lsjs4c_appToLoad'] = array
+(
+    'label'                   => &$GLOBALS['TL_LANG']['tl_layout']['lsjs4c_appToLoad'],
+    'exclude'                 => true,
+    'inputType'               => 'fileTree',
+    'eval'                    => array('multiple' => false, 'tl_class' => 'clr', 'files' => false, 'filesOnly' => false, 'fieldType' => 'radio'),
+    'sql'                     => "blob NULL"
 );
 
-$GLOBALS['TL_DCA']['tl_layout']['fields']['lsjs4c_appToLoadTextPath'] = array(
-    'sql'                   => "text NULL",
-	'label'                 => &$GLOBALS['TL_LANG']['tl_layout']['lsjs4c_appToLoadTextPath'],
-	'exclude'               => true,
-	'inputType'             => 'text',
-	'eval'                  => array(
-		'tl_class'              =>'clr'
-	)
+$GLOBALS['TL_DCA']['tl_layout']['fields']['lsjs4c_appToLoadTextPath'] = array
+(
+    'label'                   => &$GLOBALS['TL_LANG']['tl_layout']['lsjs4c_appToLoadTextPath'],
+    'exclude'                 => true,
+    'inputType'               => 'text',
+    'eval'                    => array('tl_class' => 'clr'),
+    'sql'                     => "text NULL"
 );
 
-$GLOBALS['TL_DCA']['tl_layout']['fields']['lsjs4c_appCustomizationToLoad'] = array(
-    'sql'                   => "blob NULL",
-	'label'                 => &$GLOBALS['TL_LANG']['tl_layout']['lsjs4c_appCustomizationToLoad'],
-	'exclude'               => true,
-	'inputType'             => 'fileTree',
-	'eval'                  => array(
-		'multiple'              => false,
-		'tl_class'              =>'clr',
-		'files'                 => false,
-		'filesOnly'             => false,
-		'fieldType'             => 'radio'
-	)
+$GLOBALS['TL_DCA']['tl_layout']['fields']['lsjs4c_appCustomizationToLoad'] = array
+(
+    'label'                   => &$GLOBALS['TL_LANG']['tl_layout']['lsjs4c_appCustomizationToLoad'],
+    'exclude'                 => true,
+    'inputType'               => 'fileTree',
+    'eval'                    => array('multiple' => false, 'tl_class' => 'clr', 'files' => false, 'filesOnly' => false, 'fieldType' => 'radio'),
+    'sql'                     => "blob NULL"
 );
 
-$GLOBALS['TL_DCA']['tl_layout']['fields']['lsjs4c_appCustomizationToLoadTextPath'] = array(
-    'sql'                   => "text NULL",
-    'label'                 => &$GLOBALS['TL_LANG']['tl_layout']['lsjs4c_appCustomizationToLoadTextPath'],
-    'exclude'               => true,
-    'inputType'             => 'text',
-    'eval'                  => array(
-        'tl_class'              =>'clr'
-    )
+$GLOBALS['TL_DCA']['tl_layout']['fields']['lsjs4c_appCustomizationToLoadTextPath'] = array
+(
+    'label'                   => &$GLOBALS['TL_LANG']['tl_layout']['lsjs4c_appCustomizationToLoadTextPath'],
+    'exclude'                 => true,
+    'inputType'               => 'text',
+    'eval'                    => array('tl_class' => 'clr'),
+    'sql'                     => "text NULL"
 );
 
-$GLOBALS['TL_DCA']['tl_layout']['fields']['lsjs4c_coreCustomizationToLoad'] = array(
-    'sql'                   => "blob NULL",
-	'label'                 => &$GLOBALS['TL_LANG']['tl_layout']['lsjs4c_coreCustomizationToLoad'],
-	'exclude'               => true,
-	'inputType'             => 'fileTree',
-	'eval'                  => array(
-		'multiple'              => false,
-		'tl_class'              =>'clr',
-		'files'                 => false,
-		'filesOnly'             => false,
-		'fieldType'             => 'radio'
-	)
+$GLOBALS['TL_DCA']['tl_layout']['fields']['lsjs4c_coreCustomizationToLoad'] = array
+(
+    'label'                   => &$GLOBALS['TL_LANG']['tl_layout']['lsjs4c_coreCustomizationToLoad'],
+    'exclude'                 => true,
+    'inputType'               => 'fileTree',
+    'eval'                    => array('multiple' => false, 'tl_class' => 'clr', 'files' => false, 'filesOnly' => false, 'fieldType' => 'radio'),
+    'sql'                     => "blob NULL"
 );
 
-$GLOBALS['TL_DCA']['tl_layout']['fields']['lsjs4c_coreCustomizationToLoadTextPath'] = array(
-    'sql'                   => "text NULL",
-    'label'                 => &$GLOBALS['TL_LANG']['tl_layout']['lsjs4c_coreCustomizationToLoadTextPath'],
-    'exclude'               => true,
-    'inputType'             => 'text',
-    'eval'                  => array(
-        'tl_class'              =>'clr'
-    )
+$GLOBALS['TL_DCA']['tl_layout']['fields']['lsjs4c_coreCustomizationToLoadTextPath'] = array
+(
+    'label'                   => &$GLOBALS['TL_LANG']['tl_layout']['lsjs4c_coreCustomizationToLoadTextPath'],
+    'exclude'                 => true,
+    'inputType'               => 'text',
+    'eval'                    => array('tl_class' => 'clr'),
+    'sql'                     => "text NULL"
 );
 
-$GLOBALS['TL_DCA']['tl_layout']['fields']['lsjs4c_modulesToExclude'] = array(
-    'sql'                   => "blob NULL",
-	'label'                 => &$GLOBALS['TL_LANG']['tl_layout']['lsjs4c_modulesToExclude'],
-	'exclude'               => true,
-	'inputType'             => 'fileTree',
-	'eval'                  => array(
-		'multiple'              => true,
-		'tl_class'              =>'clr',
-		'files'                 => false,
-		'filesOnly'             => false,
-		'fieldType'             => 'checkbox'
-	)
+$GLOBALS['TL_DCA']['tl_layout']['fields']['lsjs4c_modulesToExclude'] = array
+(
+    'label'                   => &$GLOBALS['TL_LANG']['tl_layout']['lsjs4c_modulesToExclude'],
+    'exclude'                 => true,
+    'inputType'               => 'fileTree',
+    'eval'                    => array('multiple' => true, 'tl_class' => 'clr', 'files' => false, 'filesOnly' => false, 'fieldType' => 'checkbox'),
+    'sql'                     => "blob NULL"
 );
 
-$GLOBALS['TL_DCA']['tl_layout']['fields']['lsjs4c_modulesToExcludeTextPath'] = array(
-    'sql'                   => "text NULL",
-    'label'                 => &$GLOBALS['TL_LANG']['tl_layout']['lsjs4c_modulesToExcludeTextPath'],
-    'exclude'               => true,
-    'inputType'             => 'text',
-    'eval'                  => array(
-        'tl_class'              =>'clr'
-    )
+$GLOBALS['TL_DCA']['tl_layout']['fields']['lsjs4c_modulesToExcludeTextPath'] = array
+(
+    'label'                   => &$GLOBALS['TL_LANG']['tl_layout']['lsjs4c_modulesToExcludeTextPath'],
+    'exclude'                 => true,
+    'inputType'               => 'text',
+    'eval'                    => array('tl_class' => 'clr'),
+    'sql'                     => "text NULL"
 );
 
-$GLOBALS['TL_DCA']['tl_layout']['fields']['lsjs4c_debugMode'] = array(
-    'sql'                   => "char(1) NOT NULL default ''",
-	'label'                 => &$GLOBALS['TL_LANG']['tl_layout']['lsjs4c_debugMode'],
-	'exclude'               => true,
-	'inputType'             => 'checkbox',
-	'eval'                  => array('tl_class'=>'m12')
+$GLOBALS['TL_DCA']['tl_layout']['fields']['lsjs4c_debugMode'] = array
+(
+    'label'                   => &$GLOBALS['TL_LANG']['tl_layout']['lsjs4c_debugMode'],
+    'exclude'                 => true,
+    'inputType'               => 'checkbox',
+    'eval'                    => array('tl_class' => 'm12'),
+    'sql'                     => "char(1) NOT NULL default ''"
 );
 
-$GLOBALS['TL_DCA']['tl_layout']['fields']['lsjs4c_noCache'] = array(
-    'sql'                   => "char(1) NOT NULL default ''",
-	'label'                 => &$GLOBALS['TL_LANG']['tl_layout']['lsjs4c_noCache'],
-	'exclude'               => true,
-	'inputType'             => 'checkbox',
-	'eval'                  => array('tl_class'=>'m12')
+$GLOBALS['TL_DCA']['tl_layout']['fields']['lsjs4c_noCache'] = array
+(
+    'label'                   => &$GLOBALS['TL_LANG']['tl_layout']['lsjs4c_noCache'],
+    'exclude'                 => true,
+    'inputType'               => 'checkbox',
+    'eval'                    => array('tl_class' => 'm12'),
+    'sql'                     => "char(1) NOT NULL default ''"
 );
 
-$GLOBALS['TL_DCA']['tl_layout']['fields']['lsjs4c_noMinifier'] = array(
-    'sql'                   => "char(1) NOT NULL default ''",
-	'label'                 => &$GLOBALS['TL_LANG']['tl_layout']['lsjs4c_noMinifier'],
-	'exclude'               => true,
-	'inputType'             => 'checkbox',
-	'eval'                  => array('tl_class'=>'m12')
+$GLOBALS['TL_DCA']['tl_layout']['fields']['lsjs4c_noMinifier'] = array
+(
+    'label'                   => &$GLOBALS['TL_LANG']['tl_layout']['lsjs4c_noMinifier'],
+    'exclude'                 => true,
+    'inputType'               => 'checkbox',
+    'eval'                    => array('tl_class' => 'm12'),
+    'sql'                     => "char(1) NOT NULL default ''"
 );
-
 
 class tl_layout_controller extends \Backend {
     public function handleCache($dc) {
