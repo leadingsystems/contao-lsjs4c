13ee69bf9b3a71b3d8ba4115c00d1ff15b037b70
Daniel Bitsch <bitsch@leadingsystems.de>
2023-10-11 16:35:25 +0200

    add toDo for Contao 5

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -4,8 +4,9 @@ namespace LeadingSystems\Lsjs4c;
 
 /*
  * Include the lsjs core in FE and BE
+ * @toDo remove TL_MODE for Contao 5
+ * use: System::getContainer()->get('contao.routing.scope_matcher')->isBackendRequest(System::getContainer()->get('request_stack')->getCurrentRequest() ?? Request::create(''))
  */
-
 if (TL_MODE === 'BE') {
     $GLOBALS['TL_HOOKS']['outputBackendTemplate'][] = array('LeadingSystems\Lsjs4c\lsjs4c_controller', 'getBackendLsjs');
 }
