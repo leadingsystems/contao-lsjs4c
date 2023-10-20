20737bcb0d73d2511f9d42d17f1ec1c27f301b18
vstuber <stuber@leadingsystems.de>
2023-10-20 23:19:51 +0200

    Adapt to changes in LSJS and don't use the LSJS feature "module blacklist" anymore.

--- src/Resources/contao/config/database.sql
+++ src/Resources/contao/config/database.sql
@@ -6,8 +6,6 @@ CREATE TABLE `tl_layout` (
 	`lsjs4c_appCustomizationToLoadTextPath` text NULL,
 	`lsjs4c_coreCustomizationToLoad` blob NULL,
 	`lsjs4c_coreCustomizationToLoadTextPath` text NULL,
-	`lsjs4c_modulesToExclude` blob NULL,
-	`lsjs4c_modulesToExcludeTextPath` text NULL,
 	`lsjs4c_debugMode` char(1) NOT NULL default '',
 	`lsjs4c_noCache` char(1) NOT NULL default '',
 	`lsjs4c_noMinifier` char(1) NOT NULL default ''
--- src/Resources/contao/dca/tl_layout.php
+++ src/Resources/contao/dca/tl_layout.php
@@ -4,7 +4,7 @@ namespace LeadingSystems\Lsjs4c;
 
 $GLOBALS['TL_DCA']['tl_layout']['config']['onsubmit_callback'][] = ['LeadingSystems\Lsjs4c\tl_layout_controller', 'handleCache'];
 
-$GLOBALS['TL_DCA']['tl_layout']['palettes']['default'] .= ';{lsjs4c_legend},lsjs4c_loadLsjs,lsjs4c_appToLoad,lsjs4c_appToLoadTextPath,lsjs4c_appCustomizationToLoad,lsjs4c_appCustomizationToLoadTextPath,lsjs4c_coreCustomizationToLoad,lsjs4c_coreCustomizationToLoadTextPath,lsjs4c_modulesToExclude,lsjs4c_modulesToExcludeTextPath,lsjs4c_debugMode,lsjs4c_noCache,lsjs4c_noMinifier';
+$GLOBALS['TL_DCA']['tl_layout']['palettes']['default'] .= ';{lsjs4c_legend},lsjs4c_loadLsjs,lsjs4c_appToLoad,lsjs4c_appToLoadTextPath,lsjs4c_appCustomizationToLoad,lsjs4c_appCustomizationToLoadTextPath,lsjs4c_coreCustomizationToLoad,lsjs4c_coreCustomizationToLoadTextPath,lsjs4c_debugMode,lsjs4c_noCache,lsjs4c_noMinifier';
 
 $GLOBALS['TL_DCA']['tl_layout']['fields']['lsjs4c_loadLsjs'] = array(
 	'label'                   => &$GLOBALS['TL_LANG']['tl_layout']['lsjs4c_loadLsjs'],
@@ -72,28 +72,6 @@ $GLOBALS['TL_DCA']['tl_layout']['fields']['lsjs4c_coreCustomizationToLoad'] = ar
 
 $GLOBALS['TL_DCA']['tl_layout']['fields']['lsjs4c_coreCustomizationToLoadTextPath'] = array(
     'label' => &$GLOBALS['TL_LANG']['tl_layout']['lsjs4c_coreCustomizationToLoadTextPath'],
-    'exclude' => true,
-    'inputType' => 'text',
-    'eval' => array(
-        'tl_class'=>'clr'
-    )
-);
-
-$GLOBALS['TL_DCA']['tl_layout']['fields']['lsjs4c_modulesToExclude'] = array(
-	'label' => &$GLOBALS['TL_LANG']['tl_layout']['lsjs4c_modulesToExclude'],
-	'exclude' => true,
-	'inputType' => 'fileTree',
-	'eval' => array(
-		'multiple' => true,
-		'tl_class'=>'clr',
-		'files' => false,
-		'filesOnly' => false,
-		'fieldType' => 'checkbox'
-	)
-);
-
-$GLOBALS['TL_DCA']['tl_layout']['fields']['lsjs4c_modulesToExcludeTextPath'] = array(
-    'label' => &$GLOBALS['TL_LANG']['tl_layout']['lsjs4c_modulesToExcludeTextPath'],
     'exclude' => true,
     'inputType' => 'text',
     'eval' => array(
